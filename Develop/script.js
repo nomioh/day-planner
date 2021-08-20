9333e4448ca460c7fe58e5a6081013eda45ef1a3
Noyemi Ohanyan <nomiohanyan@gmail.com>
2021-08-20 15:27:11 -0400

    add console logs

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,7 @@
-// time variables //
-
-// var currentDay = moment().format("MMMM Do, YYYY");
-// $("#currentDay").text(today);
-
-// console.log(today);
+console.log(document);
 
 // variable for storing schedule times
-var hours = [
+let hours = [
   {
     id: "0",
     hoursText: "9 am",
@@ -34,28 +29,45 @@ var hours = [
   {
     id: "4",
     hoursText: "1 pm",
-    time: "1",
+    time: "13",
     task: "",
   },
   {
     id: "5",
-    hoursText: "2 am",
-    time: "10",
+    hoursText: "2 pm",
+    time: "14",
     task: "",
   },
   {
-    id: "0",
-    hoursText: "9 am",
-    time: "9",
+    id: "6",
+    hoursText: "3 pm",
+    time: "15",
     task: "",
   },
   {
-    id: "1",
-    hoursText: "10 am",
-    time: "10",
+    id: "7",
+    hoursText: "4 pm",
+    time: "16",
+    task: "",
+  },
+  {
+    id: "8",
+    hoursText: "5 pm",
+    time: "17",
     task: "",
   },
 ];
+
+init();
+laodDayPlanner();
+
+function getCurrentDate() {
+  let date = moment().format("MMMM Do YYYY");
+
+  $("#currentDate").text(date);
+
+  console.log(date);
+}
 
 // var currentHour = moment().hours();
 // var meetingInfo = $("#meeting-info");
