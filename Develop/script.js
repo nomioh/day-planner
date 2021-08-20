18f91f090f6286285c6ce07ee808f07f80fac51b
Noyemi Ohanyan <nomiohanyan@gmail.com>
2021-08-20 01:15:42 -0400

    variable to store schedule and times

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,11 @@
 // time variables //
 
-var currentDay = moment().format("MMMM Do, YYYY");
-$("#currentDay").text(today);
+// var currentDay = moment().format("MMMM Do, YYYY");
+// $("#currentDay").text(today);
 
-console.log(today);
+// console.log(today);
 
+// variable for storing schedule times
 var hours = [
   {
     id: "0",
@@ -12,6 +13,49 @@ var hours = [
     time: "9",
     task: "",
   },
+  {
+    id: "1",
+    hoursText: "10 am",
+    time: "10",
+    task: "",
+  },
+  {
+    id: "2",
+    hoursText: "11 am",
+    time: "11",
+    task: "",
+  },
+  {
+    id: "3",
+    hoursText: "12 pm",
+    time: "12",
+    task: "",
+  },
+  {
+    id: "4",
+    hoursText: "1 pm",
+    time: "1",
+    task: "",
+  },
+  {
+    id: "5",
+    hoursText: "2 am",
+    time: "10",
+    task: "",
+  },
+  {
+    id: "0",
+    hoursText: "9 am",
+    time: "9",
+    task: "",
+  },
+  {
+    id: "1",
+    hoursText: "10 am",
+    time: "10",
+    task: "",
+  },
 ];
-var currentHour = moment().hours();
-var meetingInfo = $("#meeting-info");
+
+// var currentHour = moment().hours();
+// var meetingInfo = $("#meeting-info");
